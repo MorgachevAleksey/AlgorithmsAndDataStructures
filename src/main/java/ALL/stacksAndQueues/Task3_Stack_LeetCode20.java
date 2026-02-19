b9164b4df671a5d0b0f051743c1ae03f99e9a9b1
Aleksey_Morgachev <morgachev_aleksey@mail.ru>
2026-02-19 21:17:59 +0300

    v.5 Добавлен вручную реализованный стэк, для решения задачи 20 LeetCode

--- src/main/java/ALL/stacksAndQueues/Task3_Stack_LeetCode20.java
+++ src/main/java/ALL/stacksAndQueues/Task3_Stack_LeetCode20.java
@@ -1,17 +1,98 @@
 package ALL.stacksAndQueues;
 
 public class Task3_Stack_LeetCode20 {
-    //Реализовать стэк для решения 20 задачи LEETCODE
+    //Решение задачи с использованием созданного стэка
     public static void main(String[] args){
+        String s = "()";
+        int n = s.length();
+        Stack myStack = new Stack(n);
 
+        //TODO дописать решение задачи с помощью методов стэка
+        //Проход по строке скобок
+        for (int i = 0; i < n; i++){
+            //Если скобка открывающая
+            if (s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{') {
+                myStack.push(s.charAt(i));
+            }
+            //Если скобка закрывающая
+            else {
+                //Если открывающих перед ней нет
+                if (myStack.isEmpty()) {
+                    System.out.println("false");
+                }
+                //Если перед ней есть открывающие
+                else {
+                    //Если скобки совпадают
+                    if (myStack.peek() == '(' && s.charAt(i) == ')' || myStack.peek() == '[' && s.charAt(i) == ']' || myStack.peek() == '{' && s.charAt(i) == '}') {
+
+                    }
+                    //Если не совпадают
+                    else {
+
+                    }
+                }
+            }
+        }
+        //Если в конце в стэке остались открывающие скобки
+        if (!myStack.isEmpty()){
+            System.out.println("false");
+        }
     }
 
+    //Реализация стэка для решения 20 задачи LEETCODE
     static class Stack{
-        String[] arr;
-        String top;
+        char[] arr;
+        int top;
+        int size;
 
-        public Stack(String element){
+        public Stack(int n){
+            this.arr = new char[n];
+            this.top = -1;
+            this.size = n;
+        }
 
+        public void push(char element){
+            if (!isFull()) {
+                top++;
+                arr[top] = element;
+            } else{
+                resize();
+                top++;
+                arr[top] = element;
+            }
+        }
+
+        public char pop(){
+            if (!isEmpty()) {
+                return arr[top--];
+            } else {
+                return 'f';
+            }
+        }
+
+        public char peek() {
+            return arr[top];
+        }
+
+        public boolean isEmpty(){
+            return top == -1;
+        }
+
+        public boolean isFull(){
+            return top == size - 1;
+        }
+
+        public int size(){
+            return top + 1;
+        }
+
+        public void resize(){
+            char[] newArr = new char[size * 2];
+            for(int i = 0; i <= top; i++){
+                newArr[i] = arr[i];
+            }
+            arr = newArr;
+            size = size * 2;
         }
     }
 }
--- src/main/java/ALL/stacksAndQueues/Task4_Queue.java
+++ src/main/java/ALL/stacksAndQueues/Task4_Queue.java
@@ -1,4 +1,13 @@
 package ALL.stacksAndQueues;
 
 public class Task4_Queue {
+    //Реализация очереди на массиве
+    static class Queue{
+        int[] arr; //Основной массив
+        int tail; //Индекс последнего элемента
+        int size; //Текущий размер
+        int capacity; //Максимальный размер, на данный момент
+
+
+    }
 }
